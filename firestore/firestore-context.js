db4cf9a82e858960863ef19224cbe73b783020b1
marioxe301 <mariogflores16@gmail.com>
2021-10-06 20:58:18 -0600

    Automatic detection of ENV

--- firestore/firestore-context.js
+++ firestore/firestore-context.js
@@ -1,8 +1,14 @@
 const admin = require('firebase-admin');
-
-admin.initializeApp({
-    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_CRED))
-});
+if(process.env.NODE_ENV === 'develop'){
+    admin.initializeApp({
+        credential: admin.credential.cert(require(process.env. FIREBASE_JSON_DIR))
+    });
+    console.log('Running in Develop Server');
+}else{
+    admin.initializeApp({
+        credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_CRED))
+    });
+}
 
 const dbContext = admin.firestore();
 
